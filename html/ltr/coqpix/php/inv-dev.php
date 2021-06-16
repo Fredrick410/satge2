ece557db897eacb5f3432c55ee522240ea42815a
LECLERCColin <leclerccolin@gmail.com>
2021-06-16 12:57:09 +0200

    envoie fleche pas finis

--- html/ltr/coqpix/php/inv-dev.php
+++ html/ltr/coqpix/php/inv-dev.php
@@ -12,10 +12,44 @@ ini_set('display_startup_errors', TRUE);
     $pdoStat->bindValue(':id_session',$_SESSION['id_session']); //$_SESSION 
     $pdoStat->execute();
     $info = $pdoStat->fetch();
+    
+    $max_num = "";
+    $pdoSt = $bdd->prepare('SELECT id FROM devis');
+            $pdoSt->bindValue(':num',$_SESSION['id_session']); //$_SESSION
+            $pdoSt->execute(); 
+            $num = $pdoSt->fetchAll();
+            $count_num = count($num);
+            $max_num = "0";
+            for ($i=0; $i < $count_num ; $i++) { 
+                foreach($num as $n):
 
+                    $number = $n['id'];
+                    if($number > $max_num){
+                        $max_num = $number;
+                    }
+
+                endforeach;
+            }
+
+            $max_num = $max_num + 1;
+
+            $strlen_num = strlen($max_num);
+            
+            if($strlen_num == "1"){
+                $max_num = '0'.$max_num;
+            }elseif($strlen_num == "2"){
+                $max_num = '0'.$max_num;
+            }elseif($strlen_num >= "3"){
+                $max_num = $max_num;
+            }
+
+           
+    
+      $idfac = $info['id'];
       $numerosinfo = $info['numerosfacture'];   // numeros facture ou devis ect
       $dte = $info['dte']; //changer 
       $dateecheance = $info['dateecheance'];
+      $ref = $info['reffacture'];
       $nomproduit = $info['nomproduit'];
       $pour = $info['facturepour']; //changer
       $adresse = $info['adresse'];
@@ -25,17 +59,20 @@ ini_set('display_startup_errors', TRUE);
       $modalite = $info['modalite'];
       $monnaie = $info['monnaie'];
       $note = $info['note'];
+      $accompte = $info['accompte'];
       $status = $info['status_facture']; //changer
       $color = "badge badge-light-danger badge-pill";
+      $descrip = $info['descrip'];
       $etiquette = $info['etiquette'];
-
+      
     //insert
 
-    $insert = $bdd->prepare('INSERT INTO devis (numerosdevis, dte, dateecheance, nomproduit, devispour, adresse, email, tel, departement, modalite, monnaie, note, status_devis, status_color, etiquette, id_session) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+    $insert = $bdd->prepare('INSERT INTO devis (numerosdevis, dte, dateecheance, refdevis, nomproduit, devispour, adresse, email, tel, departement, modalite, monnaie, note, accompte, status_devis, status_color, etiquette, descrip, id_session) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
     $insert->execute(array(
         htmlspecialchars($numerosinfo),
         htmlspecialchars($dte),
         htmlspecialchars($dateecheance),
+        htmlspecialchars($ref),
         htmlspecialchars($nomproduit),
         htmlspecialchars($pour),
         htmlspecialchars($adresse),
@@ -45,9 +82,11 @@ ini_set('display_startup_errors', TRUE);
         htmlspecialchars($modalite),
         htmlspecialchars($monnaie),
         htmlspecialchars($note),
+        htmlspecialchars($accompte),
         htmlspecialchars($status),
         htmlspecialchars($color),
         htmlspecialchars($etiquette),
+        htmlspecialchars($descrip),
         htmlspecialchars($_SESSION['id_session']) //$_SESSION
     ));
 
@@ -64,7 +103,7 @@ ini_set('display_startup_errors', TRUE);
         
         $req = $bdd->prepare($sql);
         $req->bindValue(':num',$_SESSION['id_session']); //$_SESSION
-        $req->bindValue(':numeros',$numerosinfo); 
+        $req->bindValue(':numeros', $idfac); 
         $req->execute();
         $res = $req->fetch();
     }catch(Exception $e){
@@ -88,17 +127,22 @@ ini_set('display_startup_errors', TRUE);
         $pdo->execute();
 
         //switch article de devis a facture
+      
 
         $pdoA = $bdd->prepare('UPDATE articles SET typ="devisvente" WHERE typ="facturevente" AND numeros=:numeros AND id_session=:num');  
         $pdoA->bindValue(':num', $_SESSION['id_session']); //$_SESSION
-        $pdoA->bindValue(':numeros', $numerosinfo);
+        $pdoA->bindValue(':numeros',  $idfac);
         $pdoA->execute();
+        
+        $pdoc = $bdd->prepare('UPDATE articles SET numeros=:numeros WHERE typ="devisvente" AND numeros=:fac AND id_session=:num');  
+        $pdoc->bindValue(':num', $_SESSION['id_session']); //$_SESSION
+        $pdoc->bindValue(':numeros',  $max_num);
+        $pdoc->bindValue(':fac',  $idfac);
+        $pdoc->execute();
         
         //delete devis
 
-        $pdoDel = $bdd->prepare('DELETE FROM facture WHERE id=:id LIMIT 1');
-        $pdoDel->bindValue(':id', $_GET['id']);
-        $pdoDel->execute();
+       
         
         
         header('Location: ../app-devis-list.php');
