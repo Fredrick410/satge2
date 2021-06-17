f5fffe3405f34b2e18a1c09d473139733598c11b
robsk8 <duflotrobin7@gmail.com>
2021-06-17 14:33:21 +0200

    creation domiciliation

--- Readme.md.txt
+++ Readme.md.txt
@@ -4,3 +4,6 @@ tache réalisé 16/06/21
 implementation du site + bdd sur ma machine,
 essai de réorientation du bouton sur page-creation-chat.php
 travail sur page creation-societe
+
+tache réalisé 17/06/21
+creation de domiciliation.php
---
+++ html/ltr/coqpix/domiciliation.php
@@ -0,0 +1,225 @@
+<?php 
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+require_once 'php/config.php';
+require_once 'php/verif_session_crea.php';
+    
+    $pdoSta = $bdd->prepare('SELECT * FROM crea_societe WHERE id=:num');
+    $pdoSta->bindValue(':num',$_SESSION['id_crea']);
+    $pdoSta->execute();
+    $crea = $pdoSta->fetch();
+
+    if($crea['doc_pieceid'] == ""){
+        $doc_pieceid = "0";
+    }else{
+        $doc_pieceid = "1";
+    }
+    if($crea['doc_cerfaM0'] == ""){
+        $doc_cerfaM0 = "0";
+    }else{
+        $doc_cerfaM0 = "1";
+    }
+    if($crea['doc_justificatifd'] == ""){
+        $doc_justificatifd = "0";
+    }else{
+        $doc_justificatifd = "1";
+    }
+    if($crea['doc_affectation'] == ""){
+        $doc_affectation = "0";
+    }else{
+        $doc_affectation = "1";
+    }
+    if($crea['doc_pouvoir'] == ""){
+        $doc_pouvoir = "0";
+    }else{
+        $doc_pouvoir = "1";
+    }
+    if($crea['doc_attestation'] == ""){
+        $doc_attestation = "0";
+    }else{
+        $doc_attestation = "1";
+    }
+    if($crea['doc_xp'] == ""){
+        $doc_xp = "0";
+    }else{
+        $doc_xp = "1";
+    }
+    if($crea['doc_peirl'] == ""){
+        $doc_peirl = "0";
+    }else{
+        $doc_peirl = "1";
+    }
+?>
+<!DOCTYPE html>
+<html class="loading" lang="fr" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta name="description" content="Coqpix crée By audit action plus - développé par Youness Haddou">
+    <meta name="keywords" content="application, audit action plus, expert comptable, application facile, Youness Haddou, web application">
+    <meta name="author" content="Audit action plus - Youness Haddou">
+    <title>Création de société</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.png">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,600%7CIBM+Plex+Sans:300,400,500,600,700" rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/editors/quill/quill.snow.css">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/components.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/themes/semi-dark-layout.css">
+    <!-- END: Theme CSS-->
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/core/menu/menu-types/horizontal-menu.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/pages/app-email.css">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../assets/css/style.css">
+    <!-- END: Custom CSS-->
+
+</head>
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+
+<body class="horizontal-layout horizontal-menu navbar-sticky content-left-sidebar email-application  footer-static  " data-open="hover" data-menu="horizontal-menu" data-col="content-left-sidebar">
+<style>
+
+.nofavo{text-decoration: none; color : #c7cfd6;}
+.nofavoh:hover{text-decoration: none; color : #ffcd02;}
+.favo{text-decoration: none; color : #ffcd02;}
+.favoh:hover{text-decoration: none; color : #c7cfd6;}
+.line{text-decoration: underline;}
+.sizeright{font-size: 12px;}
+.nonedoc {display : none;}
+.esp{color: #828D99; text-decoration: underline;}
+.esp:hover{color: #34465b; text-decoration: underline;}
+
+    
+</style>
+
+
+    <!-- BEGIN: Header-->
+    <nav class="header-navbar navbar-expand-lg navbar navbar-with-menu navbar-static-top bg-secondary navbar-brand-center">
+        <div class="navbar-header d-xl-block d-none">
+            <ul class="nav navbar-nav flex-row">
+                <li class="nav-item"><a class="navbar-brand" href="#">
+                        <div class="brand-logo"><img class="logo" src="../../../app-assets/images/logo/coqpix1.png"></div>
+                    </a></li>
+            </ul>
+        </div>
+        <div class="navbar-wrapper">
+            <div class="navbar-container content">
+                <div class="navbar-collapse" id="navbar-mobile">
+                    <ul class="nav navbar-nav float-right d-flex align-items-center">                        
+                        <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
+                                <div class="user-nav d-lg-flex d-none"><span class="user-name"><?= $crea['name_crea'] ?></span><span class="user-status">En ligne</span></div><span><img class="round" src="../../../app-assets/images/ico/astro1.gif" alt="avatar" height="40" width="40"></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right pb-0">
+                                <div class="dropdown-divider mb-0"></div><a class="dropdown-item" href="php/disconnect.php"><i class="bx bx-power-off mr-50"></i> Se déconnecter</a>
+                            </div>
+                        </li>
+                        <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-expand"><i class="ficon bx bx-fullscreen"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- END: Header-->
+
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-area-wrapper">
+            <div class="sidebar-left">
+                <div class="sidebar">
+                    <div class="sidebar-content email-app-sidebar d-flex">
+                        <!-- sidebar close icon -->
+                        <span class="sidebar-close-icon">
+                            <i class="bx bx-x"></i>
+                        </span>
+                        <!-- sidebar close icon -->
+                        <div class="email-app-menu">
+                            <div class="sidebar-menu-list">
+                                <!-- sidebar menu  -->
+                                <div class="list-group list-group-messages">
+                                    
+                                </div>
+                                <!-- sidebar menu  end-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-right">
+                <div class="content-overlay"></div>
+                <div class="content-wrapper">
+                    <div class="content-header row">
+                        
+                    </div>
+                    <div class="content-body">
+                    <form action="recherche-domiciliation.php">
+                        <div class="form-group col-md-6 mb-50">
+                            <label for="inputlastname4">Entrer l'adresse de domiciliation souhaitée</label>
+                            <input type="text" name="adresse" class="form-control rounded-pill" placeholder="Adresse" required>
+                            <br>
+                            <button type="submit" class="btn btn-primary glow position-relative rounded-pill">Rechercher<i id="icon-arrow" class="bx bx-right-arrow-alt"></i></button>
+                        </div>
+                    </form>
+                        <!-- email app overlay -->
+                        <div class="app-content-overlay"></div>
+                        <div class="email-app-area">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END: Content-->
+
+    </div>
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+
+    <!-- BEGIN: Vendor JS-->
+    <script src="../../../app-assets/vendors/js/vendors.min.js"></script>
+    <script src="../../../app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.js"></script>
+    <script src="../../../app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.js"></script>
+    <script src="../../../app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="../../../app-assets/vendors/js/ui/jquery.sticky.js"></script>
+    <script src="../../../app-assets/vendors/js/editors/quill/quill.min.js"></script>
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="../../../app-assets/js/scripts/configs/horizontal-menu.js"></script>
+    <script src="../../../app-assets/js/core/app-menu.js"></script>
+    <script src="../../../app-assets/js/core/app.js"></script>
+    <script src="../../../app-assets/js/scripts/components.js"></script>
+    <script src="../../../app-assets/js/scripts/footer.js"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+
+    <!-- END: Page JS-->
+    <!-- TIMEOUT -->
+    <?php include('timeout.php'); ?>
+</body>
+<!-- END: Body-->
+
+</html>
