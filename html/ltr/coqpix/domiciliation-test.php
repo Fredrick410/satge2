681eeb77836e99f2e4c223a677c640196c72b803
Pupaslors <contrainleo@gmail.com>
2021-06-23 17:07:58 +0200

    test bouton de chat

--- html/ltr/coqpix/domiciliation-test.php
+++ html/ltr/coqpix/domiciliation-test.php
@@ -228,11 +228,245 @@ require_once 'php/verif_session_crea.php';
             </div>
         </div>
     </div>
+    
     <!-- END: Content-->
 
     </div>
     <div class="sidenav-overlay"></div>
     <div class="drag-target"></div>
+
+    <!-- BEGIN: Chat-->
+    <div class='cible' id="cible2">
+        <button class="open-button">Chat</button>
+        <button onclick="openForm()">Expand</button>
+        <div class="chat-popup" id="myForm">
+            <form action="/action_page.php" class="form-container">
+                <h1>Chat</h1>
+                <label for="msg"><b>Message</b></label>
+                <textarea placeholder="Type message.." name="msg" required></textarea>
+
+                <button type="submit" class="btn">Send</button>
+                <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+            </form>
+        </div>
+    </div>
+    <!-- END: Chat-->
+
+
+    <style>
+
+/* Button used to open the chat form - fixed at the bottom of the page */
+.open-button {
+  background-color: #555;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  opacity: 0.8;
+  position: relative;
+  width: 280px;
+}
+
+/* The popup chat - hidden by default */
+.chat-popup {
+  display: none;
+  position: absolute;
+  width: 280px;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+}
+
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+
+/* Full-width textarea */
+.form-container textarea {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+  resize: none;
+  min-height: 200px;
+}
+
+/* When the textarea gets focus, do something */
+.form-container textarea:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Set a style for the submit/send button */
+.form-container .btn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom:10px;
+  opacity: 0.8;
+}
+
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: red;
+}
+
+/* Add some hover effects to buttons */
+.form-container .btn:hover, .open-button:hover {
+  opacity: 1;
+}
+
+.cible { 
+  cursor:move;
+  /* Obligatoire sur l'élément draggable */
+  position:absolute;
+}
+ 
+/* Class ajouté quand on dragge */
+.oWdgCursorDrag{
+ 
+} 
+
+/* Position initiale */
+#cible2{right : 25%; bottom:20%}
+</style>
+
+
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+var oWdgCursor = function (sElement, sLimite) {
+  this.oLimite = null;
+  this.oElement = null;
+  this.oLimite = document.getElementById(sLimite);
+  this.bDrag = false;
+  this.bError = false;
+  this.sClassDrag = 'oWdgCursorDrag';
+  this.oPos = {x:0,y:0};
+  this.moveDiv = this.moveDiv.bind(this); 
+  this.getBoundingLimite = function(){
+    if(this.oLimite == document.documentElement){
+      return  {width:window.innerWidth, 
+               height:window.innerHeight,
+               top:this.oLimite.offsetTop,
+               left:this.oLimite.offsetLeft
+              }
+    }
+    return this.oLimite.getBoundingClientRect();
+  }
+  /**
+  * Initialise les evenements
+  */
+  this.init = function (sLimite, sElement) {  
+    this.oElement = document.getElementById(sElement); 
+    this.oLimite =(sLimite === undefined)? document.documentElement:document.getElementById(sLimite);
+    if(this.oElement == null || this.oLimite == null){
+      return true;
+    }//if
+    this.oElement.addEventListener('mousedown', this.moveDiv);
+    this.oElement.addEventListener('touchstart', this.moveDiv);
+    return false;
+  }//fct 
+
+  this.bError = this.init(sLimite, sElement);
+}//fct
+
+oWdgCursor.prototype.moveDiv  = function (oEvent){
+  oEvent.preventDefault();
+  if(oEvent.type=="touchstart" || oEvent.type=="mousedown"){
+    this.bDrag = true;
+    var oTouch = oEvent,
+        oRect = this.oElement.getBoundingClientRect();
+    if(oEvent.type=="touchstart"){
+      oTouch = null;
+      if (oEvent.targetTouches.length > 0 ) {
+        for(var i = 0; i < oEvent.targetTouches.length ; i++){
+          if(oEvent.targetTouches[i].target == this.oElement){
+            oTouch = oEvent.targetTouches[i];
+            break;
+          }//if
+        }//for
+      }//if
+      if(oTouch==null){return}
+    } //if
+    this.oPos = {'left':(oTouch.clientX - oRect.left),'top': (oTouch.clientY - oRect.top)};
+    document.addEventListener('mouseup', this.moveDiv) ;
+    this.oElement.addEventListener('mouseup', this.moveDiv) ;
+    document.addEventListener('touchend', this.moveDiv) ; 
+
+    document.addEventListener('mousemove', this.moveDiv) ; 
+    document.addEventListener('touchmove', this.moveDiv) ; 
+  }else if(oEvent.type=="touchend" || oEvent.type=="mouseup"){
+    this.bDrag = false;
+    this.oElement.classList.remove(this.sClassDrag)
+    document.removeEventListener('mousemove', this.moveDiv) ;
+    document.removeEventListener('touchmove', this.moveDiv) ;
+    document.removeEventListener('mouseup', this.moveDiv) ;
+    document.removeEventListener('touchend', this.moveDiv) ; 
+    this.oElement.removeEventListener('mouseup', this.moveDiv) ;
+  }else if(oEvent.type=="touchmove" || oEvent.type=="mousemove"){
+    var oTouch = oEvent;
+
+    if(oEvent.type=="touchmove"){
+      oTouch = null;
+      if (oEvent.targetTouches.length > 0 ) {
+        for(var i = 0; i < oEvent.targetTouches.length ; i++){
+          if(oEvent.targetTouches[i].target == this.oElement){
+            oTouch = oEvent.targetTouches[i];
+            break;
+          }//if
+        }//for
+      }//if
+      if(oTouch==null){return}
+    }//if
+    if(this.bDrag == true){ 
+      this.oElement.classList.add(this.sClassDrag)
+      var oRect = this.getBoundingLimite(),
+          iWidth= this.oElement.offsetWidth,
+          iHeight = this.oElement.offsetHeight, 
+          iClientX = oTouch.clientX - oRect.left - this.oPos.left,
+          iClientY = oTouch.clientY- oRect.top - this.oPos.top 
+      ;
+      if(iClientX < 0 ){
+        iClientX = 0;
+      }else if(iClientX + iWidth > oRect.width){
+        iClientX = oRect.width - iWidth ;
+      }
+      if(iClientY < 0 ){
+        iClientY = 0;
+      }else if(iClientY + iHeight > oRect.height){
+        iClientY = oRect.height - iHeight ;
+      } 
+      this.oElement.style.left = iClientX+'px';
+      this.oElement.style.top  = iClientY+'px';   
+    }//if
+    else{
+      this.oElement.classList.remove(this.sClassDrag)
+    }
+  }//else if
+}//fct 
+
+document.addEventListener('DOMContentLoaded',function(){  
+  var oZone2 = new oWdgCursor('cible2'); 
+});
+
+</script>
+
+
+
 
     <!-- BEGIN: Vendor JS-->
     <script src="../../../app-assets/vendors/js/vendors.min.js"></script>
