4d39969c859cb2cb4ce11128791ced2a207d3443
Ra5c0 <gigonoscar@cy-tech.fr>
2021-06-25 10:13:04 +0200

    offre domiciliation

--- html/ltr/coqpix/domiciliation-offre.php
+++ html/ltr/coqpix/domiciliation-offre.php
@@ -211,6 +211,7 @@ input[type="checkbox"].solu:checked + label {
     background-color: #29fe8c;
 }
 
+
 </style>
 
 <!-- BEGIN: Body-->
@@ -226,7 +227,8 @@ input[type="checkbox"].solu:checked + label {
     <br>
     <div class="breadcrumb-wrapper col-12" ">
         <ol class="breadcrumb p-0 mb-0" style="background-color: #ffffff;">
-            <li class="breadcrumb-item"><a href="domiciliation.php"><i class="livicon-evo" data-options=" name: arrow-left.svg; size: 26px " style="cursor: pointer; position: relative; top: 0px;"></i></a></li>
+            <li class="breadcrumb-item"><a href="page-creation.php"><i class="bx bx-home-alt"></i></a></li>
+            <li id="lien-domi" class="breadcrumb-item"><a href="domiciliation.php" style="color: #5A8DEE;">Domiciliation</a></li>
             <li class="breadcrumb-item active"> Offre de domiciliation </li>
         </ol>
     </div>
