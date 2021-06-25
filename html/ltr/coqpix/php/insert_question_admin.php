605af73212d6b8bf2f606af4f4cf9183503a59c6
belloabdoul <belloabdoul@gmail.com>
2021-06-25 09:45:28 +0200

    Ajout de page et mise a jour de pages existantes
    Ajout des pages pour le traitement d'un qcm et d'une question, modification du code existant pour le rendre dynamique

---
+++ html/ltr/coqpix/php/insert_question_admin.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'verif_session_connect_admin.php';
+require_once 'config.php';
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+    if(isset($_POST['reponses']) AND isset($_POST['vraioufaux']) AND $_POST['libelle'] != "" AND $_POST['idqcm'] != "" AND $_POST['points'] != "") {
+        $id = htmlspecialchars($_POST['idqcm']);
+        if(count($_POST['reponses']) == count($_POST['vraioufaux']) AND count($_POST['reponses']) >= 2){
+            try {
+                $insert = $bdd->prepare("INSERT INTO question(idqcm, libelle, points) VALUES(?, ?, ?)");
+                $insert->execute(array(
+                    htmlspecialchars($_POST['idqcm']),
+                    htmlspecialchars($_POST['libelle']),
+                    htmlspecialchars($_POST['points']) 
+                ));
+            } catch (PDOException $exception) {
+                var_dump($exception->getMessage());
+                echo "qcm-add-admin.php?id=$id";
+            }
+            
+            $id_question = $bdd->lastInsertId();
+            try {
+                for($i = 0; $i < count($_POST['reponses']); ++$i) {
+                        $insert = $bdd->prepare("INSERT INTO reponse (idquestion, libelle, vrai_ou_faux) VALUES(?, ?, ?)");
+                        $insert->execute(array(
+                            $id_question,
+                            htmlspecialchars($_POST['reponses'][$i]),
+                            htmlspecialchars($_POST['vraioufaux'][$i]) //$_SESSION
+                        ));
+                }
+            } catch (PDOException $exception) {
+                var_dump($exception->getMessage());
+                echo "qcm-add-admin.php?id=$id";
+            }
+            $id = $_POST['idqcm'];
+            echo "recrutement-list-question.php?id=$id";
+            exit();
+        }
+    }
+    echo "qcm-add-admin.php?id=$id";
+    exit();
+    
+?>
---
+++ html/ltr/coqpix/recrutement-list-question.php
@@ -0,0 +1,294 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+require_once 'php/config.php';
+require_once 'php/verif_session_connect_admin.php';
+
+if (!isset($_GET['id']))
+    header('Location: rh-recrutement-entretien.php');
+elseif (empty($_GET['id'])) {
+    header('Location: rh-recrutement-entretien.php');
+}
+
+$pdoStt = $bdd->prepare('SELECT * FROM qcm WHERE id = :id');
+$pdoStt->bindValue(':id', htmlspecialchars($_GET['id']));
+$pdoStt->execute();
+$qcms = $pdoStt->fetchAll(PDO::FETCH_ASSOC);
+
+if (count($qcms) != 1) {
+    header('Location: rh-recrutement-entretient.php');
+}
+
+$pdoStt = $bdd->prepare('SELECT * FROM question WHERE idqcm = :id');
+$pdoStt->bindValue(':id', htmlspecialchars($qcms[0]['id']));
+$pdoStt->execute();
+$questions = $pdoStt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($questions as $key => $value) {
+    $pdoStt = $bdd->prepare('SELECT id, libelle, vrai_ou_faux FROM reponse WHERE id = :id');
+    $pdoStt->bindValue(':id', $value['id']);
+    $pdoStt->execute();
+    $reponses[] = $pdoStt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+$pdoStt = $bdd->prepare('SELECT * FROM admin WHERE id = :numentreprise');
+$pdoStt->bindValue(':numentreprise', $_SESSION['id_admin']);
+$pdoStt->execute();
+$entreprise = $pdoStt->fetch();
+
+?>
+
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta name="description" content="Frest admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Frest admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+    <title>Recrutement - Coqpix</title>
+    <link rel="apple-touch-icon" href="../../../app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,600%7CIBM+Plex+Sans:300,400,500,600,700" rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/charts/apexcharts.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/extensions/dragula.min.css">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/components.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/themes/semi-dark-layout.css">
+    <!-- END: Theme CSS-->
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/core/menu/menu-types/horizontal-menu.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/pages/dashboard-analytics.css">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../assets/css/style.css">
+    <!-- END: Custom CSS-->
+
+</head>
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+
+<body class="horizontal-layout horizontal-menu navbar-static dark-layout 2-columns   footer-static  " data-open="hover" data-menu="horizontal-menu" data-col="2-columns" data-layout="dark-layout">
+
+    <!-- BEGIN: Header-->
+    <nav class="header-navbar navbar-expand-lg navbar navbar-with-menu navbar-static-top bg-primary navbar-brand-center">
+        <div class="navbar-header d-xl-block d-none">
+            <ul class="nav navbar-nav flex-row">
+                <li class="nav-item"><a class="navbar-brand" href="dashboard-admin.php">
+                        <div class="brand-logo"><img class="logo" src="../../../app-assets/images/logo/coqpix1.png"></div>
+                    </a></li>
+            </ul>
+        </div>
+        <div class="navbar-wrapper">
+            <div class="navbar-container content">
+                <div class="navbar-collapse" id="navbar-mobile">
+                    <ul class="nav navbar-nav float-right d-flex align-items-center">
+                        <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
+                                <div class="user-nav d-lg-flex d-none"><span class="user-name">Coqpix</span><span class="user-status">En ligne</span></div><span><img class="round" src="../../../app-assets/images/ico/astro1.gif" alt="avatar" height="40" width="40"></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right pb-0">
+                                <div class="dropdown-divider mb-0"></div><a class="dropdown-item" href="php/disconnect-admin.php"><i class="bx bx-power-off mr-50"></i> Se déconnecter</a>
+                            </div>
+                        </li>
+                        <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-expand"><i class="ficon bx bx-fullscreen"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- END: Header-->
+
+    <?php include('php/menu_backend.php'); ?>
+
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item mobile-menu d-xl-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ficon bx bx-menu"></i></a></li>
+                </ul>
+                <ul class="nav navbar-nav bookmark-icons">
+                    <li class="nav-item d-none d-lg-block"><a class="nav-link" onclick="retourn()" href="#" data-toggle="tooltip" data-placement="top" title="Retour">
+                            <div class="livicon-evo" data-options=" name: share-alt.svg; style: lines; size: 40px; strokeWidth: 2; rotate: -90"></div>
+                        </a></li>
+                </ul>
+                <script>
+                    function retourn() {
+                        window.history.back();
+                    }
+                </script>
+                <!-- qcm list -->
+                <section class="invoice-list-wrapper">
+                    <!-- create qcm button-->
+                    <div class="row">
+                        <?php
+                        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') $url = "https://";
+                        else $url = "http://";
+                        $url .= $_SERVER['HTTP_HOST'];
+                        $url .= $_SERVER['REQUEST_URI'];
+                        ?>
+
+                        <div class="col-12">
+                            <div class="h3">
+                                <?= $qcms[0]['libelle'] ?>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="invoice-create-btn mb-1">
+                                <a href="qcm-add-admin.php?id=<?= $qcms[0]['id'] ?>" class="btn btn-primary glow invoice-create" role="button" aria-pressed="true"><i class="bx bx-plus"></i>Ajouter une question</a>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="table-responsive">
+                        <table id="qcmList" class="table dt-responsive nowrap" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        Référence question
+                                    </th>
+                                    <th>Libellé</th>
+                                    <th>Points</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                for ($i = 0; $i < count($questions); ++$i) {
+                                ?>
+                                    <tr>
+                                        <td>
+                                            <?= $i + 1 ?>
+                                        </td>
+                                        <td>
+                                            <?= $questions[$i]['libelle'] ?>
+                                        </td>
+                                        <td>
+                                            <?= $questions[$i]['points'] ?>
+                                        </td>
+                                        <td>
+                                            <div class="invoice-action"><br>
+                                                <a href="#modal<?= $questions[$i]['id'] ?>" class="invoice-action-view mr-1" data-toggle="modal">
+                                                    <i class="bx bx-show-alt"></i>
+                                                </a>
+
+                                                <div class="modal fade" id="modal<?= $questions[$i]['id'] ?>" tabindex="-1" role="dialog" style="display: none;" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-centered modal-dialog-centered modal-dialog-scrollable" role="document">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title">Details</h5>
+                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                    <i class="bx bx-x"></i>
+                                                                </button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <ul>
+                                                                    <?= $questions[$i]['libelle'] ?>
+                                                                    <?php
+                                                                    var_dump($reponses);
+                                                                    foreach ($reponses as $key => $value) {
+                                                                        if ($value[0]['idquestion'] === $questions[$i]['id']) {
+                                                                            foreach ($value as $key => $val) {
+                                                                    ?>
+                                                                                <li><?= $val['libelle'] ?> : <?= $val['vrai_ou_faux'] ?></li>
+                                                                    <?php
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                    ?>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-light-secondary" data-dismiss="modal">
+                                                                    <i class="bx bx-x d-block d-sm-none"></i>
+                                                                    <span class="d-none d-sm-block">Close</span>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <a href="qcm-edit-admin.php?id=<?= $questions[$i]['id'] ?>&idqcm=<?= $qcms[0]['id'] ?>"" class=" invoice-action-edit cursor-pointer">
+                                                    <i class="bx bx-edit"></i>
+                                                </a>&nbsp;&nbsp;&nbsp;&nbsp;
+                                                <a href="php/delete_question_admin.php?id=<?= $questions[$i]['id'] ?>&idqcm=<?= $questions[$i]['idqcm'] ?>" class="invoice-action-view mr-1">
+                                                    <i class="bx bxs-trash"></i>
+                                                </a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                <?php
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+    <!-- END: Content-->
+
+    <!-- BEGIN: Footer-->
+
+    <!-- END: Footer-->
+
+
+    <!-- BEGIN: Vendor JS-->
+    <script src="../../../app-assets/vendors/js/vendors.min.js"></script>
+    <script src="../../../app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.js"></script>
+    <script src="../../../app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.js"></script>
+    <script src="../../../app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="../../../app-assets/vendors/js/ui/jquery.sticky.js"></script>
+    <script src="../../../app-assets/vendors/js/extensions/dragula.min.js"></script>
+    <script src="../../../app-assets/vendors/js/tables/datatable/datatables.min.js"></script>
+    <script src="../../../app-assets/vendors/js/tables/datatable/dataTables.bootstrap4.min.js"></script>
+    <script src="../../../app-assets/vendors/js/tables/datatable/datatables.checkboxes.min.js"></script>
+    <script src="../../../app-assets/vendors/js/tables/datatable/dataTables.responsive.min.js"></script>
+    <script src="../../../app-assets/vendors/js/tables/datatable/responsive.bootstrap.min.js"></script>
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="../../../app-assets/js/scripts/configs/horizontal-menu.js"></script>
+    <script src="../../../app-assets/js/scripts/configs/vertical-menu-dark.js"></script>
+    <script src="../../../app-assets/js/core/app-menu.js"></script>
+    <script src="../../../app-assets/js/core/app.js"></script>
+    <script src="../../../app-assets/js/scripts/components.js"></script>
+    <script src="../../../app-assets/js/scripts/footer.js"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    <script>
+        $(document).ready(function() {
+            $('#qcmList').DataTable();
+        });
+    </script>
+    <!-- END: Page JS-->
+
+</body>
+<!-- END: Body-->
+
+</html>
