0cd5f9ef66000d261fd7adf88188f4cb1b0a6461
Pupaslors <contrainleo@gmail.com>
2021-06-25 16:29:48 +0200

    amelioration du chat

--- assets/js/domiciliation_Btn_Chat.js
+++ assets/js/domiciliation_Btn_Chat.js
@@ -1,7 +1,14 @@
 function openForm() {
   document.getElementById("myForm").style.display = "block";
+  document.getElementById("icon_chat").style.display = "none";
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+  document.getElementById("icon_chat").style.display = "block";
+}
+
+function updateScroll(){
+  var element = document.getElementById("historique");
+  element.scrollTop = element.scrollHeight;
 }
--- html/ltr/coqpix/domiciliation.php
+++ html/ltr/coqpix/domiciliation.php
@@ -383,27 +383,50 @@ input[type="checkbox"].solu:checked + label {
             </div>
         </div>
 
-        <img class="open-button" id="icon_chat" src="../../../app-assets/images/ico/chat_icon.png" onclick="openForm()">
+        <img class="open-button" id="icon_chat" src="../../../app-assets/images/ico/chat_icon.png" onclick="openForm(), updateScroll()">
             <div class="chat-popup" id="myForm">
                 <form action="/action_page.php" class="form-container tri-right round btm-left">
             
                     <img class="cross" src="../../../app-assets/images/ico/cross.png" onclick="closeForm()"></img>
                     <div class="profil">
+                        <img src="../../../app-assets/images/ico/profil2.png" alt="" class="img-profil">
                         <p class="nom">Nom Prénom</p>
+                        <p class="dispo">Actif</p>
                     </div>
-
-                    <div class="historique">
-                        <div class="divSelf">
-                            <p class="msgSelf round">Moi Moi Moi Moi Moi Moi Moi Moi Moi Moi Moi Moi Moi Moi </p>
-                        </div>
-                        <div class="divYou">
-                            <p class="msgYou round">Lui Lui Lui Lui Lui Lui Lui Lui Lui Lui Lui Lui Lui Lui </p>
-                        </div>
+                    
+                    <div class="historique" id="historique">
+                        
+                            
+                            <p class="msgSelf round">Bonjour, j'aimerais obtenir quelques renseignements au sujet de la domiciliation.</p>
+                                <?php $datemsg = "5:08"; ?>
+                                <span class="chat-time right"><?= $datemsg; ?></span>
+                            
+                            <p class="msgSelf round">Bonjour</p>
+                                <?php $datemsg = "5:08"; ?>
+                                <span class="chat-time right"><?= $datemsg; ?></span>    
+                        
+                            
+                            <p class="msgYou round">Bonjour</p>
+                                <?php $dateactuelle = date("H:i"); ?>
+                                <span class="chat-time left"><?= $dateactuelle; ?></span>
+                            
+                                <p class="msgYou round">Bonjour ! Notre Service d'Aide est là pour ça !</p>
+                                <?php $dateactuelle = date("H:i"); ?>
+                                <span class="chat-time left"><?= $dateactuelle; ?></span>
+                                <p class="msgYou round">Bonjour ! Notre Service d'Aide est là pour ça !</p>
+                                <?php $dateactuelle = date("H:i"); ?>
+                                <span class="chat-time left"><?= $dateactuelle; ?></span>
+                                <p class="msgYou round">Bonjour ! Notre Service d'Aide est là pour ça !</p>
+                                <?php $dateactuelle = date("H:i"); ?>
+                                <span class="chat-time left"><?= $dateactuelle; ?></span>
+                                <p class="msgYou round">Bonjour ! Notre Service d'Aide est là pour ça !</p>
+                                <?php $dateactuelle = date("H:i"); ?>
+                                <span class="chat-time left"><?= $dateactuelle; ?></span>
                     </div>
 
                     <div class="message">
                         <textarea class="round" placeholder="Message" name="msg" required></textarea> 
-                        <button type="submit" class="btn">Send</button>
+                        <button type="submit" class="btn"><img src="../../../app-assets/images/ico/send.png" alt=""></button>
                     </div>
                 </form>
             </div>
