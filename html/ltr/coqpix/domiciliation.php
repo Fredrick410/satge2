17efd764d548a9c756d97fe6ad28f1a737a851d1
Pupaslors <contrainleo@gmail.com>
2021-06-28 09:43:03 +0200

    factorisation du chat

--- html/ltr/coqpix/domiciliation.php
+++ html/ltr/coqpix/domiciliation.php
@@ -89,7 +89,6 @@ require_once 'php/verif_session_crea.php';
 
     <!-- BEGIN: Custom CSS-->
     <link rel="stylesheet" type="text/css" href="../../../assets/css/style.css">
-    <link rel="stylesheet" type="text/css" href="../../../assets/css/domiciliation_Btn_Chat.css">
     <!-- END: Custom CSS-->
 
 </head>
@@ -382,54 +381,7 @@ input[type="checkbox"].solu:checked + label {
             
             </div>
         </div>
-
-        <img class="open-button" id="icon_chat" src="../../../app-assets/images/ico/chat_icon.png" onclick="openForm(), updateScroll()">
-            <div class="chat-popup" id="myForm">
-                <form action="/action_page.php" class="form-container tri-right round btm-left">
-            
-                    <img class="cross" src="../../../app-assets/images/ico/cross.png" onclick="closeForm()"></img>
-                    <div class="profil">
-                        <img src="../../../app-assets/images/ico/profil2.png" alt="" class="img-profil">
-                        <p class="nom">Nom Prénom</p>
-                        <p class="dispo">Actif</p>
-                    </div>
-                    
-                    <div class="historique" id="historique">
-                        
-                            
-                            <p class="msgSelf round">Bonjour, j'aimerais obtenir quelques renseignements au sujet de la domiciliation.</p>
-                                <?php $datemsg = "5:08"; ?>
-                                <span class="chat-time right"><?= $datemsg; ?></span>
-                            
-                            <p class="msgSelf round">Bonjour</p>
-                                <?php $datemsg = "5:08"; ?>
-                                <span class="chat-time right"><?= $datemsg; ?></span>    
-                        
-                            
-                            <p class="msgYou round">Bonjour</p>
-                                <?php $dateactuelle = date("H:i"); ?>
-                                <span class="chat-time left"><?= $dateactuelle; ?></span>
-                            
-                                <p class="msgYou round">Bonjour ! Notre Service d'Aide est là pour ça !</p>
-                                <?php $dateactuelle = date("H:i"); ?>
-                                <span class="chat-time left"><?= $dateactuelle; ?></span>
-                                <p class="msgYou round">Bonjour ! Notre Service d'Aide est là pour ça !</p>
-                                <?php $dateactuelle = date("H:i"); ?>
-                                <span class="chat-time left"><?= $dateactuelle; ?></span>
-                                <p class="msgYou round">Bonjour ! Notre Service d'Aide est là pour ça !</p>
-                                <?php $dateactuelle = date("H:i"); ?>
-                                <span class="chat-time left"><?= $dateactuelle; ?></span>
-                                <p class="msgYou round">Bonjour ! Notre Service d'Aide est là pour ça !</p>
-                                <?php $dateactuelle = date("H:i"); ?>
-                                <span class="chat-time left"><?= $dateactuelle; ?></span>
-                    </div>
-
-                    <div class="message">
-                        <textarea class="round" placeholder="Message" name="msg" required></textarea> 
-                        <button type="submit" class="btn"><img src="../../../app-assets/images/ico/send.png" alt=""></button>
-                    </div>
-                </form>
-            </div>
+        <?php require_once('php/chat_domiciliation.php')?>
     </div>
     <!-- END: Content-->
 
@@ -458,7 +410,6 @@ input[type="checkbox"].solu:checked + label {
     <!-- END: Theme JS-->
 
     <!-- BEGIN: Page JS-->
-    <script src="../../../assets/js/domiciliation_Btn_Chat.js"></script>
     <!-- END: Page JS-->
     <!-- TIMEOUT -->
     <?php include('timeout.php'); ?>
