bef2785d4f1b91ddfc1f3c669b159d2b66271c64
Pupaslors <contrainleo@gmail.com>
2021-06-30 17:15:49 +0200

    ajout des images domiciliation

--- html/ltr/coqpix/domiciliation-offre.php
+++ html/ltr/coqpix/domiciliation-offre.php
@@ -122,17 +122,17 @@ require_once 'php/verif_session_crea.php';
         <div class="carousel-inner">
             <div class="carousel-item active" data-mdb-interval="10000000000" id="img1">
     	
-              	<img src="../../../app-assets/images/banner/banner-1.jpg" >
+              	<img src="../../../app-assets/images/pages/offre-domiciliation/page-offre/<?php echo $result['img']; ?>-1.jpg" >
       	
             </div>
             <div class="carousel-item" data-mdb-interval="10000">
     	
-      	        <img src="../../../app-assets/images/banner/banner-2.jpg" >
+      	        <img src="../../../app-assets/images/pages/offre-domiciliation/page-offre/<?php echo $result['img']; ?>-2.jpg" >
         
             </div>
             <div class="carousel-item" data-mdb-interval="10000">
     	
-      	        <img src="../../../app-assets/images/banner/banner-3.jpg" >
+      	        <img src="../../../app-assets/images/pages/offre-domiciliation/page-offre/<?php echo $result['img']; ?>-3.jpg" >
       	
             </div>
         </div>
