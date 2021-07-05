0c073e6a28150edc6792523432a147a9138a6818
robsk8 <duflotrobin7@gmail.com>
2021-07-05 12:52:33 +0200

    factorisation

--- html/ltr/coqpix/portefeuille-social-add.php
+++ html/ltr/coqpix/portefeuille-social-add.php
@@ -5,22 +5,7 @@ ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 require_once 'php/config.php';
 require_once 'php/verif_session_connect_admin.php';
-    
-    $pdoSta = $bdd->prepare('SELECT * FROM portefeuille WHERE statut = "prospect"');
-    $pdoSta->execute();
-    $portefeuille_prospect = $pdoSta->fetchAll();
-
-    $pdoSta = $bdd->prepare('SELECT * FROM portefeuille WHERE statut = "actif"');
-    $pdoSta->execute();
-    $portefeuille_actif = $pdoSta->fetchAll();
-
-    $pdoSta = $bdd->prepare('SELECT * FROM portefeuille WHERE statut = "encours"');
-    $pdoSta->execute();
-    $portefeuille_encours = $pdoSta->fetchAll();
-
-    $pdoSta = $bdd->prepare('SELECT * FROM portefeuille WHERE statut = "passif"');
-    $pdoSta->execute();
-    $portefeuille_passif = $pdoSta->fetchAll();
+require_once 'php/portefeuille.php';
 
 ?>
 
