dfe08eee010d6d1fe53e962ad365b7ee77726aef
Ra5c0 <gigonoscar@cy-tech.fr>
2021-07-27 11:19:43 +0200

    page creation edit

---
+++ html/ltr/coqpix/php/edit_crea_entreprise.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'config.php';    
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+    $pdo = $bdd->prepare('UPDATE crea_societe SET status_crea=:status_crea WHERE id=:num LIMIT 1');
+    $pdo->bindValue(':num', $_POST['id']);
+    $pdo->bindValue(':status_crea', $_POST['status_crea']);
+    
+    $pdo->execute();
+        
+        header('Location: ../page-creation-edit.php?enregister=1');
+        
+    
+
+?>
