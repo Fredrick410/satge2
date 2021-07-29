1b8dd582a7907e1b2d3d64dda865aeb4859969a9
BELLO Abdoul Koudous <belloabdoul@gmail.com>
2021-07-29 15:03:50 +0200

    Ajout du include

--- html/ltr/coqpix/php/insert_resultat_test_qcm_candidature.php
+++ html/ltr/coqpix/php/insert_resultat_test_qcm_candidature.php
@@ -4,6 +4,7 @@ require_once 'config.php';
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
+include('mail.php');
 
 $explode = explode(';', htmlspecialchars($_POST['key']));
 
