8ebff7088ca544c9559f337b173b18a4911e6082
BELLO Abdoul Koudous <belloabdoul@gmail.com>
2021-07-29 07:28:38 +0200

    Test suivi candidat

--- html/ltr/coqpix/php/maj_suivi_candidature.php
+++ html/ltr/coqpix/php/maj_suivi_candidature.php
@@ -6,11 +6,15 @@ ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 
 $id = $_SESSION['candidat'];
+
 if($_SESSION['candidat'] == $_POST['num']){
+    echo "ok";
     if (isset($_POST['suivi']) and isset($_POST['type_suivi'])) {
+        echo "ok";
         $type_suivi = array("suivi_tel", "suivi_mail", "suivi_test_specif", "suivi_entretien");
         $suivi = array("oui", "non");
         if (in_array($_POST['type_suivi'], $type_suivi) and in_array($_POST['suivi'], $suivi)) {
+            echo "ok";
             if($_POST['suivi'] == "oui"){
                 $query = "UPDATE rh_candidature SET " .$_POST['type_suivi']. "='oui' WHERE id=?";
             }
@@ -18,14 +22,16 @@ if($_SESSION['candidat'] == $_POST['num']){
                 $query = "UPDATE rh_candidature SET " .$_POST['type_suivi']. "='non' WHERE id=?";
             }
             try {
+                echo "ok";
                 $update = $bdd->prepare($query);
                 $update->bindValue(':id', htmlspecialchars($_POST['num']), PDO::PARAM_INT);
                 $update->execute();
             } catch (PDOException $exception) {
                 var_dump($exception->getMessage());
             }
+            echo "ok";
         }
     }
 }
-header("Location: rh-recrutement-view.php?num=$id");
+//header("Location: ../rh-recrutement-view.php?num=$id");
 exit();
