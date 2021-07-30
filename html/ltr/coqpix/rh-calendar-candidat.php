a3e7a666df682d8c82d5e924db9b3f5ba4a8dfa7
BELLO Abdoul Koudous <belloabdoul@gmail.com>
2021-07-30 17:11:40 +0200

    Maj calendrier entretien

--- html/ltr/coqpix/rh-calendar-candidat.php
+++ html/ltr/coqpix/rh-calendar-candidat.php
@@ -21,7 +21,7 @@ if (isset($_GET['num']) and is_numeric($_GET['num'])) {
 }
 
 // Si la candidature n'existe pas on retourne a la liste des candidatures pour entretiens
-if (isset($candidature) == 0 and isset($candidatures) == 0) {
+if (count($candidature) == 0 and isset($candidatures)) {
     header('Location: rh-entretient-candidats.php');
 }
 
@@ -77,12 +77,6 @@ $entreprise = $pdoSta->fetch();
     <script src="../../../cuba/assets/js/jquery-3.5.1.min.js"></script>
     <!-- END: JQUERY-->
 
-    <style>
-        .none-validation {
-            display: none;
-        }
-    </style>
-
 </head>
 <!-- END: Head-->
 
@@ -94,48 +88,8 @@ $entreprise = $pdoSta->fetch();
                                                                                                                                                                                                         echo "semi-";
                                                                                                                                                                                                     } ?>dark-layout">
     <!-- BEGIN: Header-->
-    <div class="header-navbar-shadow"></div>
-    <nav class="header-navbar main-header-navbar navbar-expand-lg navbar navbar-with-menu fixed-top ">
-        <div class="navbar-wrapper">
-            <div class="navbar-container content">-
-                <div class="navbar-collapse" id="navbar-mobile">
-                    <div class="mr-auto float-left bookmark-wrapper d-flex align-items-center">
-                        <ul class="nav navbar-nav">
-                            <li class="nav-item mobile-menu d-xl-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ficon bx bx-menu"></i></a></li>
-                        </ul>
-                        <ul class="nav navbar-nav bookmark-icons">
-                            <li class="nav-item d-none d-lg-block"><a class="nav-link" href="rh-entretient-candidats.php" data-toggle="tooltip" data-placement="top" title="Retour">
-                                    <div class="livicon-evo" data-options=" name: share-alt.svg; style: lines; size: 40px; strokeWidth: 2; rotate: -90"></div>
-                                </a></li>
-                        </ul>
-                        <ul class="nav navbar-nav bookmark-icons">
-                            <li class="nav-item d-none d-lg-block"><a class="nav-link" href="file-manager.php" data-toggle="tooltip" data-placement="top" title="CloudPix">
-                                    <div class="livicon-evo" data-options=" name: cloud-upload.svg; style: filled; size: 40px; strokeColorAction: #8a99b5; colorsOnHover: darker "></div>
-                                </a></li>
-                            <li class="nav-item d-none d-lg-block"><a class="nav-link" href="#" data-toggle="tooltip" data-placement="top" title="Chat">
-                                    <div class="livicon-evo" data-options=" name: comments.svg; style: filled; size: 40px; strokeColorAction: #8a99b5; colorsOnHover: darker "></div>
-                                </a></li>
-                        </ul>
-                    </div>
-                    <ul class="nav navbar-nav float-right">
-                        <li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link" id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-fr"></i><span class="selected-language">Francais</span></a>
-                            <div class="dropdown-menu" aria-labelledby="dropdown-flag"><a class="dropdown-item" href="#" data-language="en"><i class="flag-icon flag-icon-us mr-50"></i> English</a><a class="dropdown-item" href="#" data-language="fr"><i class="flag-icon flag-icon-fr mr-50"></i> French</a><a class="dropdown-item" href="#" data-language="de"><i class="flag-icon flag-icon-de mr-50"></i> German</a><a class="dropdown-item" href="#" data-language="pt"><i class="flag-icon flag-icon-pt mr-50"></i> Portuguese</a></div>
-                        </li>
-                        <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-expand"><i class="ficon bx bx-fullscreen"></i></a></li>
-                        </li>
-                        <?php include('php/notifs_frontend.php'); ?>
-                        <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
-                                <div class="user-nav d-sm-flex d-none"><span class="user-name"><?= $entreprise['nameentreprise']; ?></span><span class="user-status text-muted">En ligne</span></div><span><img class="round" src="../../../src/img/<?= $entreprise['img_entreprise'] ?>" alt="avatar" height="40" width="40"></span>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right pb-0">
-                                <?php include('php/header_action.php')  ?>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php $btnreturn = true;
+    include('php/menu_header_front.php'); ?>
 
     <!-- END: Header-->
 
@@ -148,29 +102,11 @@ $entreprise = $pdoSta->fetch();
         <div class="content-overlay"></div>
         <div class="content-wrapper">
             <div class="content-body">
-                <div id="message">
-
-                </div>
-                <div>
-                    <?php if (!isset($candidatures)) {
-                    ?>
-                        <h4>Liste des entretiens de <?php if ($candidature['sexe_candidat'] == "homme") {
-                                                        echo "Mr";
-                                                    } else {
-                                                        echo "Mme";
-                                                    } ?> <?= $candidature['nom_candidat'] ?> - <?= $candidature['name_annonce'] ?></h4>
-                    <?php
-                    } else {
-                    ?>
-                        <h4>Liste des entretiens</h4>
-                    <?php
-                    }
-                    ?>
-                </div>
                 <!-- calendar Wrapper  -->
                 <div class="calendar-wrapper position-relative">
                     <!-- calendar app overlay -->
-                    <div class="app-content-overlay"></div>
+                    <div class="app-content-overlay">
+                    </div>
                     <!-- calendar sidebar start -->
                     <div id="sidebar" class="sidebar">
                         <div class="sidebar-new-schedule">
@@ -249,7 +185,7 @@ $entreprise = $pdoSta->fetch();
                                     </li>
                                     <li role="presentation">
                                         <div role="menuitem" data-action="toggle-start-day-1" class="dropdown-item">
-                                            <input type="checkbox" class="tui-full-calendar-checkbox-square" value="toggle-start-day-1">
+                                            <input type="checkbox" class="tui-full-calendar-checkbox-square" value="toggle-start-day-1" checked>
                                             <span class="checkbox-title"></span>
                                             <span>Lundi début de semaine</span>
                                         </div>
@@ -790,19 +726,19 @@ $entreprise = $pdoSta->fetch();
                 var iconClassName;
 
                 if (type === 'day') {
-                    type = 'Daily';
+                    type = 'Jour';
                     iconClassName = 'bx bx-calendar-alt mr-25';
                 } else if (type === 'week') {
-                    type = 'Weekly';
+                    type = 'Semaine';
                     iconClassName = 'bx bx-calendar-event mr-25';
                 } else if (options.month.visibleWeeksCount === 2) {
-                    type = '2 weeks';
+                    type = '2 semaines';
                     iconClassName = 'bx bx-calendar-check mr-25';
                 } else if (options.month.visibleWeeksCount === 3) {
-                    type = '3 weeks';
+                    type = '3 semaines';
                     iconClassName = 'bx bx-calendar-check mr-25';
                 } else {
-                    type = 'Monthly';
+                    type = 'Mois';
                     iconClassName = 'bx bx-calendar mr-25';
                 }
                 calendarTypeName.innerHTML = type;
@@ -946,13 +882,6 @@ $entreprise = $pdoSta->fetch();
         })();
 
         $(document).ready(function() {
-            <?php
-            if (isset($candidatures)) {
-            ?>
-                $('sidebar-new-schedule').hide();
-            <?php
-            }
-            ?>
             // calendar sidebar scrollbar
             if ($('.sidebar').length > 0) {
                 var sidebar = new PerfectScrollbar(".sidebar", {
