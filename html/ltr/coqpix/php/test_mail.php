f184d554c772a33da8fcd23b4229735ae121caed
BELLO Abdoul Koudous <belloabdoul@gmail.com>
2021-08-02 12:49:03 +0200

    Test spam mail

---
+++ html/ltr/coqpix/php/test_mail.php
@@ -0,0 +1,21 @@
+<?php
+$message = "Bonjour,\n\n" .
+"Vous venez de valider votre dépot de document. La prochaine etape est la réalisation du test prévu pour ce poste.\n\n" .
+"Merci d'utiliser le lien suivant pour acceder au test: lien.\n\n" .
+"Il est aussi disponible dans le cas ou vous souhaitez le faire plus tard et sera invalide dès la finalisation de votre candidature.\n\n" .
+"A bientôt !\n\n" .
+"Service des Ressources Humaines.\n\n" .
+"Envoyé par Coqpix.";
+
+$sujet = 'Votre candidature pour le poste de test au sein de test.';
+
+$mail = [
+'nom_recepteur' => "Test",
+'adresse_recepteur' => "test-ddiodz659@srv1.mail-tester.com",
+'nom_emetteur' => "Service des ressources humaines",
+'adresse_emetteur' => "rh@". $_SERVER['SERVER_NAME'],
+'sujet' => $sujet,
+'message' => $message
+];
+
+$sent = email($mail);
