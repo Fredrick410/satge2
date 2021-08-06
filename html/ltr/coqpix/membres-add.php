d156e46b181591d47832f8b458ac290134c870ed
lucas.faget <lucas.faget@etu.iut-tlse3.fr>
2021-08-06 12:55:14 +0200

    modification profil + corrections bugs affichage pages membres

--- html/ltr/coqpix/membres-add.php
+++ html/ltr/coqpix/membres-add.php
@@ -36,9 +36,7 @@ require_once 'php/config.php';
 
     <!-- BEGIN: Vendor CSS-->
     <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/vendors.min.css">
-    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/plugins/forms/validation/form-validation.css">
-    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/forms/select/select2.min.css">
-    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/pickers/pickadate/pickadate.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/tables/datatable/datatables.min.css">
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
@@ -51,7 +49,7 @@ require_once 'php/config.php';
     <!-- END: Theme CSS-->
 
     <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/core/menu/menu-types/horizontal-menu.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/core/menu/menu-types/vertical-menu.css">
     <link rel="stylesheet" type="text/css" href="../../../app-assets/css/pages/page-users.css">
     <!-- END: Page CSS-->
 
@@ -67,149 +65,12 @@ require_once 'php/config.php';
 <body class="vertical-layout vertical-menu-modern <?php if($entreprise['theme_web'] == "light"){echo "semi-";} ?>dark-layout 2-columns  navbar-sticky footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="2-columns" data-layout="<?php if($entreprise["theme_web"] == "light"){echo "semi-";} ?>dark-layout">
 
     <!-- BEGIN: Header-->
-    <nav class="header-navbar navbar-expand-lg navbar navbar-with-menu navbar-static-top bg-primary navbar-brand-center">
-        <div class="navbar-header d-xl-block d-none">
-            <ul class="nav navbar-nav flex-row">
-                <li class="nav-item"><a class="navbar-brand" href="dashboard-analytics.php">
-                        <div class="brand-logo"><img class="logo" src="../../../app-assets/images/logo/coqpix1.png"></div>
-                    </a></li>
-            </ul>
-        </div>
-        <div class="navbar-wrapper">
-            <div class="navbar-container content">
-                <div class="navbar-collapse" id="navbar-mobile">
-                    <div class="mr-auto float-left bookmark-wrapper d-flex align-items-center">
-                        <ul class="nav navbar-nav bookmark-icons">
-                            <li class="nav-item d-none d-lg-block"><a class="nav-link" href="file-manager.php" data-toggle="tooltip" data-placement="top" title="Cloudpix"><div class="livicon-evo" data-options=" name: cloud-upload.svg; style: filled; size: 40px; strokeColorAction: #8a99b5; colorsOnHover: darker "></div></a></li>
-                        </ul>
-                    </div>
-                    <ul class="nav navbar-nav float-right">
-                        <li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link" id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-fr"></i><span class="selected-language">Francais</span></a>
-                            <div class="dropdown-menu" aria-labelledby="dropdown-flag"><a class="dropdown-item" href="#" data-language="en"><i class="flag-icon flag-icon-us mr-50"></i> English</a><a class="dropdown-item" href="#" data-language="fr"><i class="flag-icon flag-icon-fr mr-50"></i> French</a><a class="dropdown-item" href="#" data-language="de"><i class="flag-icon flag-icon-de mr-50"></i> German</a><a class="dropdown-item" href="#" data-language="pt"><i class="flag-icon flag-icon-pt mr-50"></i> Portuguese</a></div>
-                        </li>
-                        <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-expand"><i class="ficon bx bx-fullscreen"></i></a></li>
-                        </li>
-                        <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label" href="#" data-toggle="dropdown"><i class="ficon bx bx-bell bx-tada bx-flip-horizontal"></i><span class="badge badge-pill badge-danger badge-up"></span></a>   <!--NOTIFICATION-->
-                            <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
-                                <li class="dropdown-menu-header">
-                                    <div class="dropdown-header px-1 py-75 d-flex justify-content-between"><span class="notification-title">0 Notifications</span><span class="text-bold-400 cursor-pointer">Notification non lu</span></div>
-                                </li>
-                                <li class="scrollable-container media-list"><a class="d-flex justify-content-between" href="javascript:void(0)">
-                                                            <!-- CONTENUE ONE -->
-                                    </a>
-                                    <div class="d-flex justify-content-between cursor-pointer">
-                                        <div class="media d-flex align-items-center border-0">
-                                            <div class="media-left pr-0">
-                                                <div class="avatar mr-1 m-0"><img src="../../../app-assets/images/ico/astro1.gif" alt="avatar" height="39" width="39"></div>
-                                            </div>
-                                            <div class="media-body">
-                                                <h6 class="media-heading"><span class="text-bold-500">Nouveaux compte</span> création du compte</h6><small class="notification-text">Aujourd'hui, 19h30</small>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="dropdown-menu-footer"><a class="dropdown-item p-50 text-primary justify-content-center" href="javascript:void(0)">Tout marquer comme lu</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
-                            <div class="user-nav d-sm-flex d-none"><span class="user-name"><?= $entreprise['nameentreprise']; ?></span><span class="user-status text-muted">En ligne</span></div><span><img class="round" src="../../../src/img/<?= $entreprise['img_entreprise'] ?>" alt="avatar" height="40" width="40"></span>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right pb-0">
-                                <?php include('php/header_action.php') ?>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php $btnreturn = false;
+    include('php/menu_header_front.php'); ?>
     <!-- END: Header-->
 
     <!-- BEGIN: Main Menu-->
-    <div class="header-navbar navbar-expand-sm navbar navbar-horizontal navbar-sticky navbar-dark navbar-without-dd-arrow" role="navigation" data-menu="menu-wrapper">
-        <div class="navbar-header d-xl-none d-block">
-            <ul class="nav navbar-nav flex-row">
-                <li class="nav-item mr-auto"><a class="navbar-brand" href="index.html">
-                        <div class="brand-logo"><img class="logo" src="../../../app-assets/images/logo/coqpix1.png" /></div>
-                        <h2 class="brand-text mb-0">Coqpix</h2>
-                    </a></li>
-                <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="bx bx-x d-block d-xl-none font-medium-4 primary toggle-icon"></i></a></li>
-            </ul>
-        </div>
-        <div class="shadow-bottom"></div>
-        <!-- Horizontal menu content-->
-        <div class="navbar-container main-menu-content" data-menu="menu-container">
-            <!-- include ../../../includes/mixins-->
-            <ul class="nav navbar-nav" id="main-menu-navigation" data-menu="menu-navigation" data-icon-style="lines">
-                <li class="dropdown nav-item" data-menu="dropdown"><a class="dropdown-toggle nav-link" href="index.html" data-toggle="dropdown"><i class="menu-livicon" data-icon="rocket"></i><span data-i18n="Dashboard">Dashboard</span></a>
-                    <ul class="dropdown-menu">
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="dashboard-analytics.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>Analytique</a>
-                        </li>
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="#dashboard-ecommerce.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>eCommerce</a>
-                        </li>
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="file-manager.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>CloudPix</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="dropdown nav-item" data-menu="dropdown"><a class="dropdown-toggle nav-link" href="#" data-toggle="dropdown"><i class="menu-livicon" data-icon="briefcase"></i><span>Fonctions</span></a>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown dropdown-submenu" data-menu="dropdown-submenu"><a class="dropdown-item align-items-center dropdown-toggle" href="#" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="coins"></i>Ventes</a>
-                            <ul class="dropdown-menu">
-                                <li data-menu=""><a class="dropdown-item align-items-center" href="app-devis-list.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>Devis</a>
-                                </li>
-                                <li data-menu=""><a class="dropdown-item align-items-center" href="app-invoice-list.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>Factures</a>
-                                </li>
-                                <li data-menu=""><a class="dropdown-item align-items-center" href="app-avoir-list.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>Avoirs</a>
-                                </li>
-                                <li data-menu=""><a class="dropdown-item align-items-center" href="app-bon-list.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>Bon de livraison</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown dropdown-submenu" data-menu="dropdown-submenu"><a class="dropdown-item align-items-center dropdown-toggle" href="#" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="us-dollar"></i>Achats</a>
-                            <ul class="dropdown-menu">
-                                <li data-menu=""><a class="dropdown-item align-items-center" href="app-invoice-achat-list.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>Facture</a>
-                                </li>
-                                <li data-menu=""><a class="dropdown-item align-items-center" href="app-bon-achat-list.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>Bulletin de commande</a>
-                                </li>
-                                <li data-menu=""><a class="dropdown-item align-items-center" href="app-note-list.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>Note de frais</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown dropdown-submenu" data-menu="dropdown-submenu"><a class="dropdown-item align-items-center dropdown-toggle" href="#" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="users"></i>Projet</a>
-                            <ul class="dropdown-menu">
-                                <li data-menu=""><a class="dropdown-item align-items-center" href="#projet.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>Equipes&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span class="badge badge-light-primary badge-pill badge-round float-right">SOON</span></a>
-                                </li>
-                                <li data-menu=""><a class="dropdown-item align-items-center" href="#task.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i>Taches&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span class="badge badge-light-primary badge-pill badge-round float-right">SOON</span></a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="#inventaire-list.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="box-add"></i>Inventaire&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span class="badge badge-light-primary badge-pill badge-round float-right">SOON</span></a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="dropdown nav-item" data-menu="dropdown"><a class="dropdown-toggle nav-link" href="#" data-toggle="dropdown"><i class="menu-livicon" data-icon="priority-low"></i><span>Données</span></a>
-                    <ul class="dropdown-menu">
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="#client.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="user"></i>Clients</a>
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="fournisseur-list.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="truck"></i>Fournisseurs</a>
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="#article-liste.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="tag"></i>Article</a>
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="#membres-list.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="grid"></i>Membres</a>
-                    </ul>
-                </li>
-                <li class="dropdown nav-item" data-menu="dropdown"><a class="dropdown-toggle nav-link" href="#" data-toggle="dropdown"><i class="menu-livicon" data-icon="shield"></i><span>Editions</span></a>
-                    <ul class="dropdown-menu">
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="#Bilan.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="notebook"></i>Bilan&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span class="badge badge-light-primary badge-pill badge-round float-right">SOON</span></a>
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="#balance.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="balance"></i>Balance&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span class="badge badge-light-primary badge-pill badge-round float-right">SOON</span></a>
-                    </ul>
-                </li>
-                <li class="dropdown nav-item" data-menu="dropdown"><a class="dropdown-toggle nav-link" href="#" data-toggle="dropdown"><i class="menu-livicon" data-icon="timer"></i><span>Déclarations</span></a>
-                    <ul class="dropdown-menu">
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="#sociale.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="umbrella"></i>Sociale&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span class="badge badge-light-primary badge-pill badge-round float-right">SOON</span></a>
-                        <li data-menu=""><a class="dropdown-item align-items-center" href="#fiscale.php" data-toggle="dropdown"><i class="bx bx-right-arrow-alt"></i><i class="menu-livicon" data-icon="piggybank"></i>Fiscale&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span class="badge badge-light-primary badge-pill badge-round float-right">SOON</span></a>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        <!-- /horizontal menu content-->
+    <?php include('php/menu_front.php'); ?>
     </div>
     <!-- END: Main Menu-->
 
--- html/ltr/coqpix/php/edit_profile_1.php
+++ html/ltr/coqpix/php/edit_profile_1.php
@@ -5,30 +5,42 @@ error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 
-        $update = $bdd->prepare('UPDATE entreprise SET nameentreprise = ?, numerossiret = ?, pays_entreprise = ?, adresseentreprise = ?, descr_entreprise = ?, telentreprise = ?, link_website = ?, emailentreprise = ?, datecreation = ?, datedecloture = ?, iban_entreprise = ?, nom_diri = ?, prenom_diri = ?, adresse_diri = ?, telentreprise = ?, email_diri = ? WHERE id = ?');
-        $update->execute(array(
-            htmlspecialchars($_GET['nameentreprise']),
-            htmlspecialchars($_GET['numerossiret']),
-            htmlspecialchars($_GET['pays_entreprise']),
-            htmlspecialchars($_GET['adresseentreprise']),
-            htmlspecialchars($_GET['descr_entreprise']),
-            htmlspecialchars($_GET['telentreprise']),
-            htmlspecialchars($_GET['link_website']),
-            htmlspecialchars($_GET['emailentreprise']),
-            htmlspecialchars($_GET['datecreation']),
-            htmlspecialchars($_GET['datedecloture']),
-            htmlspecialchars($_GET['iban_entreprise']),
-            htmlspecialchars($_GET['nom_diri']),
-            htmlspecialchars($_GET['prenom_diri']),
-            htmlspecialchars($_GET['adresse_diri']),
-            htmlspecialchars($_GET['tel_diri']),
-            htmlspecialchars($_GET['email_diri']),
-            htmlspecialchars($_GET['numentreprise'])
-        ));
+        $update_entreprise = $bdd->prepare('UPDATE entreprise SET nameentreprise = :nameentreprise, 
+                                                                  numerossiret = :numerossiret,
+                                                                  pays_entreprise = :pays,
+                                                                  adresseentreprise = :adresse,
+                                                                  descr_entreprise = :descr,
+                                                                  telentreprise = :tel,
+                                                                  link_website = :website,
+                                                                  emailentreprise = :email,
+                                                                  datecreation = :datecreation,
+                                                                  datedecloture = :datecloture,
+                                                                  iban_entreprise = :iban
+                                                                  WHERE id = :id_entreprise');
+        $update_entreprise->bindValue(':nameentreprise', htmlspecialchars($_GET['nameentreprise']));
+        $update_entreprise->bindValue(':numerossiret', htmlspecialchars($_GET['numerossiret']));
+        $update_entreprise->bindValue(':pays', htmlspecialchars($_GET['pays_entreprise']));
+        $update_entreprise->bindValue(':adresse', htmlspecialchars($_GET['adresseentreprise']));
+        $update_entreprise->bindValue(':descr', htmlspecialchars($_GET['descr_entreprise']));
+        $update_entreprise->bindValue(':tel', htmlspecialchars($_GET['telentreprise']));
+        $update_entreprise->bindValue(':website', htmlspecialchars($_GET['link_website']));
+        $update_entreprise->bindValue(':email', htmlspecialchars($_GET['emailentreprise']));
+        $update_entreprise->bindValue(':datecreation', htmlspecialchars($_GET['datecreation']));
+        $update_entreprise->bindValue(':datecloture', htmlspecialchars($_GET['datedecloture']));
+        $update_entreprise->bindValue(':iban', htmlspecialchars($_GET['iban_entreprise']));
+        $update_entreprise->bindValue(':id_entreprise', htmlspecialchars($_GET['numentreprise']));
+        $update_entreprise->execute();
+
+        $update_membre = $bdd->prepare('UPDATE membres SET nom = :nom, prenom = :prenom, tel = :tel, email = :email WHERE id = :id_membre');
+        $update_membre->bindValue(':nom', htmlspecialchars($_GET['nom_membre']));
+        $update_membre->bindValue(':prenom', htmlspecialchars($_GET['prenom_membre']));
+        $update_membre->bindValue(':tel', htmlspecialchars($_GET['tel_membre']));
+        $update_membre->bindValue(':email', htmlspecialchars($_GET['email_membre']));
+        $update_membre->bindValue(':id_membre', htmlspecialchars($_GET['id_membre']));
+        $update_membre->execute();
 
         sleep(1);
         header('Location: ../page-user-profile.php');
         die();
     
-
 ?>
