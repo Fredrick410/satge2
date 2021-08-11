a7b5983cd9410df2b61bc51e8b93f9e0d920af2a
Pupaslors <contrainleo@gmail.com>
2021-08-11 15:06:59 +0200

    signature souris

--- html/ltr/coqpix/signature-contrat.php
+++ html/ltr/coqpix/signature-contrat.php
@@ -94,16 +94,43 @@ require_once 'php/verif_session_crea.php';
                     <div class="col" style="border-right: 1px solid grey;">
                         <h4 class="m-1" style="font-weight: bold;">Lire et signer le contrat</h5>
                         <div class="form-group" style="width: 500px; margin: 50px auto; text-align: center;">
-                            <form action="generate-pdf-4.php" method="POST">
-                              <div class="form-check my-3">
+                            <!-- Content -->
+	                        <div class="container">
+	                        	<div class="row">
+	                        		<div class="col-md-12">
+	                        			<h5>Signature :</h5>
+	                        		</div>
+	                        	</div>
+	                        	<div class="row">
+	                        		<div class="col-md-12">
+	                        	 		<canvas id="sig-canvas" width="400" height="150">
+	                        	 			Get a better browser, bro.
+	                        	 		</canvas>
+	                        	 	</div>
+	                        	</div>
+	                        	<div class="row">
+	                        		<div class="col-md-12">
+	                        			
+	                        			<button class="btn btn-default" id="sig-clearBtn">Effacer la signature</button>
+	                        		</div>
+	                        	</div>
+	                        	<br/>
+	                        	
+	                        	<br/>
+	                        	<div class="row">
+	                        		<div class="col-md-12">
+	                        			<img id="sig-image" width="200" height="75" src="" alt="Ta signature s'affichera ici !" hidden/>
+	                        		</div>
+	                        	</div>
+	                        </div>
+                            <form action="generate-pdf-4.php" id="form-signature" method="POST" enctype="multipart/form-data">
+                              <textarea id="sig-dataUrl" class="form-control" rows="5" name="signature" form="form-signature" hidden required></textarea>
+                              
+                              <div class="form-check my-1">
                                 <input type="checkbox" name="condition" class="form-check-input" id="condition"  required>
-                                <label for="condition" class="form-check-label" style="margin-left: 10px;">Je reconnais avoir lu et accepté les conditions générales de prestations</label>
+                                <label for="condition" class="form-check-label" style="margin-left: 10px;">Je reconnais avoir lu et accepté les conditions générales de prestations du présent contrat</label>
                               </div>
-                              <h5 style="font-weight: bold;">Signature :</h6><br>
-                                <input type="text" name="signature" id="signature" class="w-100 mb-3 py-1" style="text-align: center;" placeholder="Nom et prénom" required  onkeyup="appercu.innerHTML=this.value">
-                                <label for="appercu" class="mr-2" style="color: #051441; font-family: mukta malar bold; font-size: 15px;">Aperçu :</label>
-                                <span id="appercu" readonly></span><br>
-                                <button class="btn btn-signature mt-3" type="submit" >Valider la signature</button>
+                              <button class="btn btn-signature" type="submit" >Valider</button>
                             </form>
                         </div>
                     </div>
@@ -123,7 +150,140 @@ require_once 'php/verif_session_crea.php';
 
     <script>
 
+(function() {
+  window.requestAnimFrame = (function(callback) {
+    return window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimaitonFrame ||
+      function(callback) {
+        window.setTimeout(callback, 1000 / 60);
+      };
+  })();
 
+  var canvas = document.getElementById("sig-canvas");
+  var ctx = canvas.getContext("2d");
+  ctx.strokeStyle = "#222222";
+  ctx.lineWidth = 2;
+
+  var drawing = false;
+  var mousePos = {
+    x: 0,
+    y: 0
+  };
+  var lastPos = mousePos;
+
+  canvas.addEventListener("mousedown", function(e) {
+    drawing = true;
+    lastPos = getMousePos(canvas, e);
+  }, false);
+
+  canvas.addEventListener("mouseup", function(e) {
+    drawing = false;
+  }, false);
+
+  canvas.addEventListener("mousemove", function(e) {
+    mousePos = getMousePos(canvas, e);
+  }, false);
+
+  // Add touch event support for mobile
+  canvas.addEventListener("touchstart", function(e) {
+
+  }, false);
+
+  canvas.addEventListener("touchmove", function(e) {
+    var touch = e.touches[0];
+    var me = new MouseEvent("mousemove", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+    });
+    canvas.dispatchEvent(me);
+  }, false);
+
+  canvas.addEventListener("touchstart", function(e) {
+    mousePos = getTouchPos(canvas, e);
+    var touch = e.touches[0];
+    var me = new MouseEvent("mousedown", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+    });
+    canvas.dispatchEvent(me);
+  }, false);
+
+  canvas.addEventListener("touchend", function(e) {
+    var me = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(me);
+  }, false);
+
+  function getMousePos(canvasDom, mouseEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+      x: mouseEvent.clientX - rect.left,
+      y: mouseEvent.clientY - rect.top
+    }
+  }
+
+  function getTouchPos(canvasDom, touchEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+      x: touchEvent.touches[0].clientX - rect.left,
+      y: touchEvent.touches[0].clientY - rect.top
+    }
+  }
+
+  function renderCanvas() {
+    if (drawing) {
+      ctx.moveTo(lastPos.x, lastPos.y);
+      ctx.lineTo(mousePos.x, mousePos.y);
+      ctx.stroke();
+      lastPos = mousePos;
+    }
+  }
+
+  // Prevent scrolling when touching the canvas
+  document.body.addEventListener("touchstart", function(e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+  document.body.addEventListener("touchend", function(e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+  document.body.addEventListener("touchmove", function(e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+
+  (function drawLoop() {
+    requestAnimFrame(drawLoop);
+    renderCanvas();
+  })();
+
+  function clearCanvas() {
+    canvas.width = canvas.width;
+  }
+
+  // Set up the UI
+  var sigText = document.getElementById("sig-dataUrl");
+  var sigImage = document.getElementById("sig-image");
+  var clearBtn = document.getElementById("sig-clearBtn");
+  var submitBtn = document.getElementById("sig-submitBtn");
+  clearBtn.addEventListener("click", function(e) {
+    clearCanvas();
+    sigText.innerHTML = "Data URL for your signature will go here!";
+    sigImage.setAttribute("src", "");
+  }, false);
+  canvas.addEventListener("click", function(e) {
+    var dataUrl = canvas.toDataURL();
+    sigText.innerHTML = dataUrl;
+    sigImage.setAttribute("src", dataUrl);
+  }, false);
+
+})();
 
     </script>
 
