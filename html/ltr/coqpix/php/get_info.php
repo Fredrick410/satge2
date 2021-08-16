da12e251ed14111aa5c6ab37b3e414f2b36e33a4
CYTech Student <contrainleo@gmail.com>
2021-08-16 12:46:15 +0200

    verif adresse entreprise

--- html/ltr/coqpix/php/get_info.php
+++ html/ltr/coqpix/php/get_info.php
@@ -44,6 +44,16 @@ if($crea['adresse_entreprise'] == ""){
 }else{
     $cp_diri = "1";
 }
+if($crea['ville_entreprise'] == ""){
+    $ville_diri = "0";
+}else{
+    $ville_diri = "1";
+}
+if($crea['cp_entreprise'] == ""){
+    $cp_diri = "0";
+}else{
+    $cp_diri = "1";
+}
 if($crea['status_crea'] == ""){
     $status_crea = "0";
 }else{
