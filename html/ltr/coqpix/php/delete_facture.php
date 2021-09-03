27ae7ba43f8122c261ccd70798ca6638e85f78d7
clara95974 <annaelle.degard@laposte.net>
2021-09-03 17:11:22 +0200

    suppression/mise en commentaire des prestations sur la plupart des page + resolution bug mineure pages INVENTAIRE/VENTES

--- html/ltr/coqpix/php/delete_facture.php
+++ html/ltr/coqpix/php/delete_facture.php
@@ -1,5 +1,5 @@
 <?php
-    
+
 session_start();
 require_once 'config.php';
 error_reporting(E_ALL);
@@ -11,9 +11,6 @@ ini_set('display_startup_errors', TRUE);
         $pdoDel = $bdd->prepare('DELETE FROM articles WHERE numeros= ""');
         $pdoDel->execute();
 
-        $pdoDele = $bdd->prepare('DELETE FROM prestations WHERE numeros= ""');
-        $pdoDele->execute();
-
         //calculs
 
         $pdoS = $bdd->prepare('SELECT * FROM calculs WHERE id_session = :num');
@@ -22,19 +19,19 @@ ini_set('display_startup_errors', TRUE);
         $calculs = $pdoS->fetch();
 
         try{
-  
+
         $sql = "SELECT SUM(T.TOTAL) as MONTANT_T FROM ( SELECT cout,quantite ,(cout * quantite ) as TOTAL FROM articles WHERE id_session = :num AND numeros=:numeros AND typ='facturevente' ) T ";
-        
+
         $req = $bdd->prepare($sql);
         $req->bindValue(':num',$_SESSION['id_session']); //$_SESSION
-        $req->bindValue(':numeros',$_GET['numfacture']); 
+        $req->bindValue(':numeros',$_GET['numfacture']);
         $req->execute();
         $res = $req->fetch();
         }catch(Exception $e){
             echo "Erreur " . $e->getMessage();
         }
 
-        $montant_t = !empty($res) ? $res['MONTANT_T'] : 0;       
+        $montant_t = !empty($res) ? $res['MONTANT_T'] : 0;
 
         $facture_nb = $calculs['facture_nb'] - 1;
         $facture_all = $calculs['facture_all'] - $montant_t;
@@ -42,7 +39,7 @@ ini_set('display_startup_errors', TRUE);
         $lastdte = date('d-m-Y');  // $calculs['lastdte'] pour autre de facture
 
         $pdo = $bdd->prepare('UPDATE calculs SET facture_nb=:facture_nb, facture_all=:facture_all, devis_all=:devis_all, lastdte=:lastdte WHERE id_session=:num LIMIT 1');
-    
+
         $pdo->bindValue(':num', $_SESSION['id_session']); //$_SESSION
         $pdo->bindValue(':facture_nb', $facture_nb);
         $pdo->bindValue(':facture_all', $facture_all);
@@ -57,10 +54,6 @@ ini_set('display_startup_errors', TRUE);
         $pdoDel->bindValue(':id_session', $_SESSION['id_session']); //$_SESSION
         $pdoDel->execute();
 
-        $pdoDele = $bdd->prepare('DELETE FROM prestations WHERE numeros=:num AND id_session =:id_session AND typ="facturevente"');
-        $pdoDele->bindValue(':num', $_GET['numfacture']);
-        $pdoDele->bindValue(':id_session', $_SESSION['id_session']); //$_SESSION
-        $pdoDele->execute();
 
         $pdoDe = $bdd->prepare('DELETE FROM facture WHERE id=:id AND id_session=:id_session');
         $pdoDe->bindValue(':id', $_GET['id']);
@@ -70,5 +63,5 @@ ini_set('display_startup_errors', TRUE);
         sleep(1);
         header('Location: ../app-invoice-list.php');
         exit();
-    
+
 ?>
